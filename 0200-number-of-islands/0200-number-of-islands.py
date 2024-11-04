7bbe064ec2616e9d480d4b286d73dc9bfbee62b5
doganazcol <123711476+doganazcol@users.noreply.github.com>
2024-11-04 08:49:48 -0800

    Time: 233 ms (46.36%), Space: 30.8 MB (18.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,37 +5,31 @@ class Solution(object):
         :rtype: int
         """
 
-        #if grid is empty return 0 islands
-        if not grid:
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visit = set()
+        
         islands = 0
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
 
         def bfs(r, c):
-            q = collections.deque()
-            visit.add((r, c))
+            q = deque()
+            visited.add((r, c))
             q.append((r, c))
-            
+
             while q:
-                row, col = q.pop()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-                
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
                 for dr, dc in directions:
-                    nr, nc = row + dr, col + dc
-                    if (nr in range(rows) and
-                        nc in range(cols) and
-                        grid[nr][nc] == "1" and
-                        (nr, nc) not in visit):
-                        q.append((nr, nc))
-                        visit.add((nr, nc))
+                    r, c = row + dr, col + dc
+                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
+                        q.append((r, c))
+                        visited.add((r, c))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    bfs(r, c)
+                if grid[r][c] == "1" and (r, c) not in visited:
                     islands += 1
+                    bfs(r, c)
 
         return islands
 
