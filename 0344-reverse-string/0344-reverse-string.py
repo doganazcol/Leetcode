617857ce6dc987a6cfc3cc11dae94636f6e45d86
doganazcol <123711476+doganazcol@users.noreply.github.com>
2024-11-04 20:24:12 -0800

    Time: 1 ms (68.56%), Space: 19 MB (86.8%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        #initialize
+        l = 0 
+        r = len(s) - 1
+
+        while l < r: 
+
+            #reverse
+            temp = s[r] 
+            s[r] = s[l]
+            s[l] = temp
+
+            #update opinters
+            l += 1
+            r -= 1
+
+        return s
