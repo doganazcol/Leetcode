097001dcf82285b49f4dbfdab13d94871409e34b
doganazcol <123711476+doganazcol@users.noreply.github.com>
2024-11-13 09:26:56 -0800

    Time: 2 ms (54.23%), Space: 13.5 MB (66.17%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        def dfs(node, curr):
+            if not node:
+                return False
+            
+            # if both children are null, then the node is a leaf
+            if node.left == None and node.right == None:
+                return (curr + node.val) == targetSum
+            
+            curr += node.val
+            left = dfs(node.left, curr)
+            right = dfs(node.right, curr)
+            return left or right
+        
+        return dfs(root, 0)
+        
